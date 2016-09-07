6d981df3eb97b0de1cfdc338f774db522c924f83
benjipott <pott.benjamin@gmail.com>
2016-09-07 15:25:08 +0200

    build sources

--- es5/js/setup.js
+++ es5/js/setup.js
@@ -72,7 +72,7 @@ var autoSetup = function autoSetup() {
         }
         // Make sure this player hasn't already been set up.
         if (mediaEl.koment === undefined) {
-          var options = mediaEl.getAttribute('data-setup');
+          var options = mediaEl.getAttribute('data-setup') || wrapper && wrapper.getAttribute && wrapper.getAttribute('data-setup');
           // Check if data-setup attr exists.
           // We only auto-setup if they've added the data-setup attr.
           //if (options !== null) {
